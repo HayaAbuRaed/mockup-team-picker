fc2e8d76a7331e356d442684dfcbc202e7ef3b1e
Haya Abu-Raed <hayaa.aburaed@gmail.com>
2024-05-01 02:59:29 +0300

    fix some styles

--- src/pages/TeamPicker/TeamPicker.jsx
+++ src/pages/TeamPicker/TeamPicker.jsx
@@ -1,10 +1,11 @@
 import React from "react";
 import MemberPicker from "./components/MemberPicker/MemberPicker";
+import styles from "./TeamPicker.module.css";
 
 const TeamPicker = () => {
   return (
     <>
-      <h3> Add members to Front-end development team </h3>
+      <h3 className={styles.h3}> Add members to Front-end development team </h3>
 
       <MemberPicker />
     </>
